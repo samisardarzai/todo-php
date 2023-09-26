c0f1c1313c6f033c8d1781c00db52a668fdafb9a
Sami Sardarzai <cleversam07@gmail.com>
2023-09-26 03:25:14 -0700

    developed full app

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php  
 $insert = false;
+$update = false;
+$delete = false;
    // Connect to the Database 
 $servername = "localhost";
 $username = "root";
@@ -13,24 +15,50 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
   die("Sorry we failed to connect: ". mysqli_connect_error());
 }
+
+if(isset($_GET['delete'])){
+  $snu = $_GET['delete'];
+  $delete = true;
+  $sql = "DELETE FROM `notes1` WHERE `snu` = $snu";
+  $result = mysqli_query($conn, $sql);
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-  $title = $_POST["title"];
-  $description = $_POST["description"];
-  
+if (isset( $_POST['snuEdit'])){
+  // Update the record
+    $snu = $_POST["snuEdit"];
+    $title = $_POST["titleEdit"];
+    $description = $_POST["descriptionEdit"];
+
+  // Sql query to be executed
+  $sql = "UPDATE `notes1` SET `title` = '$title' , `description` = '$description' WHERE `notes1`.`snu` = $snu";
+  $result = mysqli_query($conn, $sql);
+  if($result){
+    $update = true;
+}
+else{
+    echo "We could not update the record successfully";
+}
+}
+else{
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
   // Sql query to be executed
   $sql = "INSERT INTO `notes1` (`title`, `description`) VALUES ('$title', '$description')";
   $result = mysqli_query($conn, $sql);
-  
-  // Add a new trip to the Trip table in the database
-  if($result){
-    $insert = true;
+
+   
+  if($result){ 
+      $insert = true;
   }
   else{
       echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
-  }
+  } 
 }
+}
+?>
 
- ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -43,7 +71,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body class="">
-     <!-- Edit Modal -->
+    
+  <!-- Edit Modal -->
   <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -54,9 +83,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <form action="/crud/index.php?update=true" method="POST">
+        <form action="/crud/index.php" method="POST">
           <div class="modal-body">
-            <input type="hidden" name="snoEdit" id="snoEdit">
+            <input type="hidden" name="snuEdit" id="snuEdit">
             <div class="form-group">
               <label for="title">Note Title</label>
               <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
@@ -75,87 +104,110 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       </div>
     </div>
   </div>
-    <nav class="navbar navbar-expand-lg  bg-body-tertiary" data-bs-theme="dark" >
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Todo Php</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact us</a>
-              </li>
-             
-          </div>
-        </div>
-      </nav>
-      <?php
-                if($insert){
-                  echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-                  <strong>Success!</strong> Your note has been inserted successfully
-                  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                    <span aria-hidden='true'>×</span>
-                  </button>
-                </div>";
-                }
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><img src="/crud/logo.svg" height="28px" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">iNote </span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+
+      </ul>
+      
+    </div>
+  </nav>
+
+  <?php
+  if($insert){
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your note has been inserted successfully
+    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>";
+  }
   ?>
+  <?php
+  if($delete){
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your note has been deleted successfully
+    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>";
+  }
+  ?>
+  <?php
+  if($update){
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>Success!</strong> Your note has been updated successfully
+    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+      <span aria-hidden='true'>×</span>
+    </button>
+  </div>";
+  }
+  ?>
+  <div class="container my-4">
+    <h2>Add a Note to inotes1</h2>
+    <form action="/crud/index.php" method="POST">
+      <div class="form-group">
+        <label for="title">Note Title</label>
+        <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+      </div>
 
-       <div class="container my-5 ">
-        <form action="/crud/index.php " method="POST" >
-            <h2>iNotes </h2>
-            <div class="form-group">
-              <label for="title">Note Title</label>
-              <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp" >
-              
-            </div>
-            <div class="form-group">
-                <label for="desc">Note Description</label>
-                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-              </div>
-            
-            <button type="submit" class="btn btn-primary my-3 ">Add Note</button>
-          </form>
-       </div>
+      <div class="form-group">
+        <label for="desc">Note Description</label>
+        <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Add Note</button>
+    </form>
+  </div>
 
-       <div class="container my-4">
-       <table class="table" id="myTable">
-  <thead>
-    <tr>
-      <th scope="col">S.No</th>
-      <th scope="col">Title</th>
-      <th scope="col">Description</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-       <?php 
-        $sql= "SELECT * FROM `notes1`" ;
-        $result = mysqli_query($conn, $sql);
-        $snu=0;
-        while($row = mysqli_fetch_assoc($result)){
-          $snu= $snu+1;
-          echo  "<tr>
-          <th scope='row'>" . $snu. "</th>
-          <td>". $row['title'] ."</td>
-          <td>". $row['description'] ."</td>
-          <td> <button class='edit btn btn-sm btn-primary' id=".$row['snu'].">Edit</button> <button class='delete btn btn-sm btn-primary' id=d".$row['snu'].">Delete</button>  </td>
+  <div class="container my-4">
+
+
+    <table class="table" id="myTable">
+      <thead>
+        <tr>
+          <th scope="col">S.No</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php 
+          $sql = "SELECT * FROM `notes1`";
+          $result = mysqli_query($conn, $sql);
+          $snu = 0;
+          while($row = mysqli_fetch_assoc($result)){
+            $snu = $snu + 1;
+            echo "<tr>
+            <th scope='row'>". $snu . "</th>
+            <td>". $row['title'] . "</td>
+            <td>". $row['description'] . "</td>
+            <td> <button class='edit btn btn-sm btn-primary' id=".$row['snu'].">Edit</button> <button class='delete btn btn-sm btn-primary' id=d".$row['snu'].">Delete</button>  </td>
           </tr>";
-        
-      }
-       ?>
-  
-  </tbody>
-</table>
+        } 
+          ?>
 
-       </div>
-       <hr>
+
+      </tbody>
+    </table>
+  </div>
+  <hr>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
@@ -166,5 +218,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
    });
  </script>
+  <script>
+    edits = document.getElementsByClassName('edit');
+    Array.from(edits).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("edit ");
+        tr = e.target.parentNode.parentNode;
+        title = tr.getElementsByTagName("td")[0].innerText;
+        description = tr.getElementsByTagName("td")[1].innerText;
+        console.log(title, description);
+        titleEdit.value = title;
+        descriptionEdit.value = description;
+        snuEdit.value = e.target.id;
+        console.log(e.target.id)
+        $('#editModal').modal('toggle');
+      })
+    })
+
+    deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("edit ");
+        snu = e.target.id.substr(1);
+
+        if (confirm("Are you sure you want to delete this note!")) {
+          console.log("yes");
+          window.location = `/crud/index.php?delete=${snu}`;
+        
+        }
+        else {
+          console.log("no");
+        }
+      })
+    })
+  </script>
   </body>
 </html>
