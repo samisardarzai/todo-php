2b9962f7c15e72f1ff2cb49836369ed587654775
Sami Sardarzai <cleversam07@gmail.com>
2023-09-26 02:35:59 -0700

    add new changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php  
-// INSERT INTO `notes` (`snu`, `title`, `description`, `tstamp`) VALUES ('', 'bookssdkfjs', 'asd,fasodfiasd', current_timestamp());
+$insert = false;
    // Connect to the Database 
 $servername = "localhost";
 $username = "root";
@@ -13,6 +13,37 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
   die("Sorry we failed to connect: ". mysqli_connect_error());
 }
+//  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+//      $title= $_POST["title"];
+//      $description = $_POST["description"]
+    
+//       $sql = "INSERT INTO `notes1` (`title`, `description`, ) VALUES ('$title','$description')";
+//       $result = mysqli_query($conn,$sql);
+
+//      if($result){
+//       echo "the record add";
+//      }
+//      else {
+//       echo "not add ";
+//       mysqli_error($conn);
+//      }
+//  }
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $title = $_POST["title"];
+  $description = $_POST["description"];
+  
+  // Sql query to be executed
+  $sql = "INSERT INTO `notes1` (`title`, `description`) VALUES ('$title', '$description')";
+  $result = mysqli_query($conn, $sql);
+  
+  // Add a new trip to the Trip table in the database
+  if($result){
+    $insert = true;
+  }
+  else{
+      echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+  }
+}
 
  ?>
 <!doctype html>
@@ -20,10 +51,13 @@ if (!$conn){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+   
     <title>iNote App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="">
     <nav class="navbar navbar-expand-lg  bg-body-tertiary" data-bs-theme="dark" >
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Todo Php</a>
@@ -45,8 +79,19 @@ if (!$conn){
           </div>
         </div>
       </nav>
+      <?php
+                if($insert){
+                  echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                  <strong>Success!</strong> Your note has been inserted successfully
+                  <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>×</span>
+                  </button>
+                </div>";
+                }
+  ?>
+
        <div class="container my-5 ">
-        <form>
+        <form action="/crud/index.php " method="POST" >
             <h2>iNotes </h2>
             <div class="form-group">
               <label for="title">Note Title</label>
@@ -55,27 +100,52 @@ if (!$conn){
             </div>
             <div class="form-group">
                 <label for="desc">Note Description</label>
-                <textarea class="form-control" name="desc" id="desc" rows="3"></textarea>
+                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
               </div>
             
             <button type="submit" class="btn btn-primary my-3 ">Add Note</button>
           </form>
        </div>
 
-       <div class="container">
-
+       <div class="container my-4">
+       <table class="table" id="myTable">
+  <thead>
+    <tr>
+      <th scope="col">S.No</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
        <?php 
-        $sql= "SELECT * FROM `notes`" ;
+        $sql= "SELECT * FROM `notes1`" ;
         $result = mysqli_query($conn, $sql);
         while($row = mysqli_fetch_assoc($result)){
-          // echo var_dump($row);
-          echo $row['sno'] .  ". title". $row['title'] ." desc id  ". $row['description'];
-          echo "<br>";
+          echo  "<tr>
+          <th scope='row'>" . $row['snu']. "</th>
+          <td>". $row['title'] ."</td>
+          <td>". $row['description'] ."</td>
+          <td>actions</td>
+          </tr>";
+        
       }
        ?>
+  
+  </tbody>
+</table>
 
        </div>
+       <hr>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+   
+   <script>
+   $(document).ready(function () {
+     $('#myTable').DataTable();
+
+   });
+ </script>
   </body>
 </html>
